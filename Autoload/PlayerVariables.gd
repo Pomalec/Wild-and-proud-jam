0c1d9a0acf510393e3eb1ac84d8a374f05c85a69
JediBoat <121259910+JediBoat@users.noreply.github.com>
2024-06-22 20:11:28 +0100

    tilemap updates

--- Autoload/PlayerVariables.gd
+++ Autoload/PlayerVariables.gd
@@ -1,14 +1,12 @@
 extends Node
 
-signal flag_changed(flag,value)
-
 #Quest related variables
 var flags = {
 	'day' : 0 , #starts at 1, with 3 being the final preparation day.
 	'energy' : 1,  #energy for the current day. resets to 3 at the start of a new day
 	
 	# MAGE STUFF
-	'ingredient_quest_heard' : false, #whether the player has met the mage or not.
+	'spoke_to_mage' : false, #whether the player has met the mage or not.
 	'ingredient_quest_done' : 0 #0 is not, 1 is half help with one energy, 2 is full help with 2 energy
 }
 
@@ -30,4 +28,3 @@ func set_flag(id:String, value):
 		
 	flags[id] = value
 	print('Flag %s set to %s' % [id, value])
-	flag_changed.emit(id,value)
--- Player.gd
+++ Player.gd
@@ -10,6 +10,6 @@ const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
-	velocity = direction * SPEED
+	velocity = direction * 600
 
 	move_and_slide()
