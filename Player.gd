9f1e184433ea7fcc3fe6da5a19ae519abb50120e
Pomalec <spoma2002@gmail.com>
2024-06-22 14:00:28 +0100

    adding some assests and player mov

---
+++ Player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+
+
+
+func _physics_process(delta):
+	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
+	velocity = direction * 600
+
+	move_and_slide()
